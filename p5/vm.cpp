7558c0fdf7e825312abaebbbee4cf1f3326feaa3
Kevin <kxlow@users.noreply.github.com>
2015-12-04 22:28:57 -0800

    vm stop

--- p5/vm.cpp
+++ p5/vm.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define PAGE_TABLE_SIZE	16
 #define FRAME_SIZE		4
 
-struct clock
+struct RAM
 {
 	unsigned int pages[FRAME_SIZE];
 	unsigned short r[FRAME_SIZE];
@@ -20,6 +20,35 @@ struct pageTable
 	unsigned int address;
 	unsigned short physicalPage;
 };
+
+void printRAM(ofstream& output, RAM RAM)
+{
+	for (int i = 0; i < FRAME_SIZE; i++)
+	{
+		if (RAM.pages[i])
+		{
+			if (i > 0)
+				output << " ";
+			output << hex << setfill('0') << setw(8) << (RAM.pages[i] & 0xFFFFF000);
+		}
+	}
+	output << endl;
+}
+
+unsigned int getPageIndex(pageTable pageTable[PAGE_TABLE_SIZE], unsigned int address)
+{
+	unsigned int pageIndex = -1;
+	unsigned int prefix = address & 0xFFFFF000; // first 12 bits are offset
+	for (int i = 0; i < PAGE_TABLE_SIZE; i++)
+	{
+		if (pageTable[i].address == prefix)
+		{
+			pageIndex = i;
+			break;
+		}
+	}
+	return pageIndex;
+}
 
 int main(int argc, char** argv)
 {
@@ -45,9 +74,24 @@ int main(int argc, char** argv)
 	}
 
 	pageTable pageTable[PAGE_TABLE_SIZE];
+	for (int i = 0; i < PAGE_TABLE_SIZE; i++)
+	{
+		pageTable[i].entryNumber = 0;
+		pageTable[i].present = 0;
+		pageTable[i].address = 0;
+		pageTable[i].physicalPage = 0;
+	}
+
+	RAM RAM;
+	for (int i = 0; i < FRAME_SIZE; i++)
+	{
+		RAM.pages[i] = 0;
+		RAM.r[i] = 0;
+	}
+	RAM.pointer = 0;
 
 	unsigned int address;
-	
+
 	// get page addresses
 	for (int i = 0; i < PAGE_TABLE_SIZE; i++)
 	{
@@ -57,32 +101,49 @@ int main(int argc, char** argv)
 
 	while (file >> hex >> address)
 	{
-		cout << hex << address << endl;
-		
 		// check if page is in ram
-		unsigned int pageIndex = -1;
-		unsigned int prefix = address & 0xFFFFF000;
-		for (int i = 0; i < FRAME_SIZE; i++)
-		{
-			if (pageTable[i].address == prefix)
-			{
-				pageIndex = i;
-				break;
-			}
-		}
+		unsigned int pageIndex = getPageIndex(pageTable, address);
 
 		if (pageIndex == -1)
 		{
 			continue;
 		}
 
+		unsigned int entryNumber;
 		if (pageTable[pageIndex].present)
 		{
-			cout << "Page is present" << endl;
+			entryNumber = pageTable[pageIndex].entryNumber;
+			RAM.r[entryNumber] = 1;
 		}
 		else
 		{
-			cout << "Page is not present" << endl;
+			while (1)
+			{
+				RAM.r[RAM.pointer] = !RAM.r[RAM.pointer];
+				if (RAM.r[RAM.pointer])
+				{
+					// clear present bit of old page
+					unsigned int oldPagePrefix = getPageIndex(pageTable, RAM.pages[RAM.pointer]);
+					pageTable[oldPagePrefix].present = 0;
+
+					// set present bit of new page and place page in RAM
+					pageTable[pageIndex].present = 1;
+					pageTable[pageIndex].entryNumber = RAM.pointer;
+					RAM.pages[RAM.pointer] = address;
+
+					// increment pointer and break
+					RAM.pointer = (RAM.pointer + 1) % FRAME_SIZE;
+					break;
+				}
+				else
+				{
+					RAM.r[RAM.pointer] = 0;
+					// increment pointer
+					RAM.pointer = (RAM.pointer + 1) % FRAME_SIZE;
+				}
+			}
 		}
+
+		printRAM(outputFile, RAM);
 	}
 }
